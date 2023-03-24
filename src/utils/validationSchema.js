a9c7973ba982efe02de2faaf50bcd9a8fcb8fe47
Yuliia Dikun <dikun.yulia@gmail.com>
2023-03-24 16:40:41 +0200

    fix: schema validation

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -12,13 +12,18 @@ export const userSchema = yup.object().shape({
   email: yup
     .string()
     .matches(
+      // eslint-disable-next-line
       /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/,
       "invalid email"
     )
     .required("required field"),
   phone: yup
     .string()
-    .matches(/^[\+]{0,1}380([0-9]{9})$/, "invalid number")
+    .matches(
+      // eslint-disable-next-line
+      /^[\+]{0,1}380([0-9]{9})$/,
+      "invalid number"
+    )
     .required("required field"),
   position_id: yup.number().required("required field").oneOf([1, 2, 3, 4]),
   photo: yup
